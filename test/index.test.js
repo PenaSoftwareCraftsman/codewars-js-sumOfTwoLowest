710de2c4aa8d0920dc60a5a40a1c5748824bb54d
PenaSoftwareCraftsman <jose.henrique.pena.goulart@gmail.com>
2023-07-31 18:09:51 -0300

    test: test refactored

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const { sumTwoSmallestNumbers } = require("./../src/index");
 
 describe('sumTwoSmallestNumbers', () => {
     it('Should return the the number 1 when the array [1, 2, 3, 4] are sended', () => {
-        let numbers = [1, 2, 3, 4];
+        let numbers = [4, 3, 2, 1];
         let result = 1
         expect(sumTwoSmallestNumbers(numbers)).toBe(result);
     })
