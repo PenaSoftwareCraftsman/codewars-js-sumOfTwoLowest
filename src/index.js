ed747742e2156325fdc7cda0b2ad99dfb27da832
PenaSoftwareCraftsman <jose.henrique.pena.goulart@gmail.com>
2023-07-31 18:08:27 -0300

    feat: Finding the lowest numbers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-const sumTwoSmallestNumbers = () => {
+const sumTwoSmallestNumbers = (numbers) => {
+    let result = numbers[0];
+    for(let number of numbers) {
+        result = number < result ? number : result;
+    }
 
+    return result;
 }
 
 module.exports = {
