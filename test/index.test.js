abe10d90d43d0ef2511215d24e7227ba6af7d567
PenaSoftwareCraftsman <jose.henrique.pena.goulart@gmail.com>
2023-07-31 17:37:56 -0300

    test: Created a test to found the lowest number
    Should return the the number 1 when the array [1, 2, 3, 4] are sended

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,9 @@
 const { sumTwoSmallestNumbers } = require("./../src/index");
 
 describe('sumTwoSmallestNumbers', () => {
-    
+    it('Should return the the number 1 when the array [1, 2, 3, 4] are sended', () => {
+        let numbers = [1, 2, 3, 4];
+        let result = 1
+        expect(sumTwoSmallestNumbers(numbers)).toBe(result);
+    })
 });
